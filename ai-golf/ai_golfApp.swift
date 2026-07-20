096c7fc83dcf66f3944adf5c6049689922d48858
Olaf Hubert Bykowski <olafbykowski1@gmail.com>
2026-07-20 19:38:02 +0200

    Initial Commit

---
+++ ai-golf/ai_golfApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_golfApp.swift
+//  ai-golf
+//
+//  Created by Olaf Bykowski on 20/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ai_golfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ai-golfTests/ai_golfTests.swift
@@ -0,0 +1,19 @@
+//
+//  ai_golfTests.swift
+//  ai-golfTests
+//
+//  Created by Olaf Bykowski on 20/07/2026.
+//
+
+import Testing
+@testable import ai_golf
+
+struct ai_golfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
